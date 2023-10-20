e285179cd40e0054fd6ce99aba1523ccc3c4a855
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-20 06:03:29 +0330

    implement OrderStatus.java

---
+++ src/main/java/com/example/phase2/entity/enumeration/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.example.phase2.entity.enumeration;
+
+public enum OrderStatus {
+    WAITING_FOR_THE_SUGGESTION_OF_EXPERTS,
+    WAITING_FOR_SELECT_NEXPERT,
+    WAITING_FOR_THE_EXPERT_TO_COME_TO_YOUR_PLACE,
+    DONE,
+    PAID
+}
