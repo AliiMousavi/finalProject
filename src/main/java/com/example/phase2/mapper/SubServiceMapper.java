9d507b5a69c6977e4fba40e0b55a69489614e7d8
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-03 06:18:01 +0330

    implement SubServiceMapper.java

---
+++ src/main/java/com/example/phase2/mapper/SubServiceMapper.java
@@ -0,0 +1,18 @@
+package com.example.phase2.mapper;
+
+import com.example.phase2.dto.*;
+import com.example.phase2.entity.ServiceCo;
+import com.example.phase2.entity.SubService;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface SubServiceMapper {
+    SubServiceMapper INSTANCE = Mappers.getMapper(SubServiceMapper.class);
+
+    SubService dtoToSubService(SubServiceRequestDto subServiceRequestDto);
+
+    SubServiceResponseDto subServiceToDto(SubService subService);
+    SubServiceResponseDto_justExpert subServiceTojustExpertDto(SubService subService);
+
+}
