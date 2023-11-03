b2e11e5beb7be1c3b4c2009531ddc25e8e46af9d
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-03 06:01:06 +0330

    implement BankCardRequestDto.java

---
+++ src/main/java/com/example/phase2/dto/BankCardRequestDto.java
@@ -0,0 +1,30 @@
+package com.example.phase2.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.format.annotation.NumberFormat;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BankCardRequestDto {
+    @NotBlank
+    @Size(min = 16, max = 16, message = "secondPassword length must be 16.")
+    String creditCardNumber;
+    @NotBlank
+//    @NumberFormat
+    @Size(min = 3, max = 4, message = "secondPassword length must be between 3 and 4 characters.")
+    String cvv2;
+//    @NumberFormat
+    @Size(min = 6, max = 12, message = "secondPassword length must be between 6 and 12 characters.")
+    @NotBlank
+    String secondPassword;
+}
