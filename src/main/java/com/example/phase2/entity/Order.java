367c693487972c39dc86339b92d53f7475a604a6
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-03 06:12:35 +0330

    Add toString() to Order.java

--- src/main/java/com/example/phase2/entity/Order.java
+++ src/main/java/com/example/phase2/entity/Order.java
@@ -3,43 +3,55 @@ package com.example.phase2.entity;
 
 import com.example.phase2.entity.enumeration.OrderStatus;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Getter
+@Setter
+@Builder
 @Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    protected Long id;
-    int offerPrice;
-    String workToDo;
-    Date DateOfExecution;
-    String address;
-    OrderStatus orderStatus;
-    @OneToOne
-    Customer customer;
-    @OneToOne
-    SubService subServiceCollection;
-    @OneToOne
-    Offer offer;
+    private Long id;
+    private int offerPrice;
+    private String workToDo;
+    private LocalDateTime DateOfExecution;
+    private String address;
+    private OrderStatus orderStatus;
+    @ManyToOne
+    private Customer customer;
+    @ManyToOne
+    private SubService subService;
+    @OneToMany(mappedBy = "order", cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    private List<Offer> offers= new ArrayList<>();
+    @ManyToOne
+    private Expert expert;
 
-    public Order(int offerPrice, String workToDo, Date dateOfExecution, String address, SubService subService) {
-        if (offerPrice > subService.getBasePrice())
-            this.offerPrice = offerPrice;
-        else
-            throw new RuntimeException("offer PRice should bigger than basePrice");
-
-        this.workToDo = workToDo;
-        DateOfExecution = dateOfExecution;
-        this.address = address;
-        this.orderStatus = OrderStatus.WAITING_FOR_THE_SUGGESTION_OF_EXPERTS;
-        this.subServiceCollection = subService;
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", offerPrice=" + offerPrice +
+                ", workToDo='" + workToDo + '\'' +
+                ", DateOfExecution=" + DateOfExecution +
+                ", address='" + address + '\'' +
+                ", orderStatus=" + orderStatus +
+                ", customer=" + customer.getFirstName() +
+                ", subService=" + subService.getName() +
+                ", offers=" + offers.stream()
+                .map(Offer::getId)
+                .toList() +
+                ", expert=" + expert.getFirstName() +
+                '}';
     }
 }
