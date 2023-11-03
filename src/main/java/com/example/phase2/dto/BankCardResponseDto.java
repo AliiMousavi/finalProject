a8a50edbbc109938e45756ce723a2388230ab372
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-03 06:01:21 +0330

    implement BankCardResponseDto.java

---
+++ src/main/java/com/example/phase2/dto/BankCardResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.phase2.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.format.annotation.NumberFormat;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BankCardResponseDto {
+    @NotBlank
+//    @CreditCardNumber
+//    @Min(16)
+//    @Max(16)
+    String creditCardNumber;
+}
