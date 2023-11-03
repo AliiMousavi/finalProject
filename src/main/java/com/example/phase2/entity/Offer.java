2203077b898e7170163e5765ba9d1827a2df5e01
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-03 06:10:37 +0330

    add toString() to Offer.java

--- src/main/java/com/example/phase2/entity/Offer.java
+++ src/main/java/com/example/phase2/entity/Offer.java
@@ -1,27 +1,44 @@
 package com.example.phase2.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Getter
+@Setter
+@Builder
 @Table(name = "offers")
 public class Offer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    protected Long id;
-    Date DateOfTheOffer;
-    int offeredprice;
-    Date offeredDate;
-    int Duration;
-    @OneToOne(mappedBy = "offer")
-    Order order;
+    private Long id;
+    private LocalDateTime DateOfOfferRegister;
+    private int offeredPrice;
+    private LocalDateTime offeredDate;
+    private int duration;
+    @ManyToOne
+    private Order order;
+    @ManyToOne
+    Expert expert;
 
-
+    @Override
+    public String toString() {
+        return "Offer{" +
+                "id=" + id +
+                ", DateOfOfferRegister=" + DateOfOfferRegister +
+                ", offeredPrice=" + offeredPrice +
+                ", offeredDate=" + offeredDate +
+                ", duration=" + duration +
+                ", order=" + order.getId() +
+                ", expert=" + expert.getFirstName() +
+                '}';
+    }
 }
