309711d76f16a89490bfb224ca65c2a225281245
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-03 06:18:47 +0330

    add @Transactional to methods in SubServiceServiceImpl.java

--- src/main/java/com/example/phase2/service/impl/SubServiceServiceImpl.java
+++ src/main/java/com/example/phase2/service/impl/SubServiceServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.phase2.entity.SubService;
 import com.example.phase2.repository.SubServiceRepository;
 import com.example.phase2.service.SubServiceService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,31 +17,37 @@ public class SubServiceServiceImpl implements SubServiceService {
     }
 
     @Override
+    @Transactional
     public SubService saveOrUpdate(SubService subService) {
         return subServiceRepository.save(subService);
     }
 
     @Override
+    @Transactional
     public Optional<SubService> findById(Long id) {
         return subServiceRepository.findById(id);
     }
 
     @Override
+    @Transactional
     public List<SubService> findAll() {
         return subServiceRepository.findAll();
     }
 
     @Override
+    @Transactional
     public List<SubService> saveAll(List<SubService> subServices) {
         return subServiceRepository.saveAll(subServices);
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
         subServiceRepository.deleteById(id);
     }
 
     @Override
+    @Transactional
     public SubService updateBasePrice(Long subServiceId, int newBasePrice) {
         SubService byId = subServiceRepository.findById(subServiceId).orElseThrow();
         byId.setBasePrice(newBasePrice);
@@ -48,6 +55,7 @@ public class SubServiceServiceImpl implements SubServiceService {
     }
 
     @Override
+    @Transactional
     public SubService updateCaption(Long subServiceId, String newCaption) {
         SubService byId = subServiceRepository.findById(subServiceId).orElseThrow();
         byId.setCaption(newCaption);
