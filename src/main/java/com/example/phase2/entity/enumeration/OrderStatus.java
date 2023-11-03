2002a0b20f47a1422a200d3d3981b13c1e1a454e
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-03 06:14:33 +0330

    add OrderStatus.BEGINNING

--- src/main/java/com/example/phase2/entity/enumeration/OrderStatus.java
+++ src/main/java/com/example/phase2/entity/enumeration/OrderStatus.java
@@ -2,8 +2,9 @@ package com.example.phase2.entity.enumeration;
 
 public enum OrderStatus {
     WAITING_FOR_THE_SUGGESTION_OF_EXPERTS,
-    WAITING_FOR_SELECT_NEXPERT,
+    WAITING_FOR_SELECT_EXPERT,
     WAITING_FOR_THE_EXPERT_TO_COME_TO_YOUR_PLACE,
+    BEGINNING,
     DONE,
     PAID
 }
