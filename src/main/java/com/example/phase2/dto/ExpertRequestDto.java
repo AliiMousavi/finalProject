685ff09c4d14b6996525196ac090b9079db91bd0
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-03 06:07:48 +0330

    implement ExpertRequestDto.java

---
+++ src/main/java/com/example/phase2/dto/ExpertRequestDto.java
@@ -0,0 +1,27 @@
+package com.example.phase2.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExpertRequestDto {
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{8}$",
+            message = "password length must be: 8 and combination of letters and numbers")
+    private String password;
+    @NotBlank
+    @Email(message = "email must have a valid regex")
+    private String email;
+}
