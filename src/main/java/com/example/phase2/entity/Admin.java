81f5fd8a2fefa29404674dcfab3e06c038fe3c94
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-03 05:58:00 +0330

    comment the singleton implementation in Admin.java

--- src/main/java/com/example/phase2/entity/Admin.java
+++ src/main/java/com/example/phase2/entity/Admin.java
@@ -5,20 +5,20 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class Admin extends User{
-    private static Admin instance;
-
-    protected Admin() {
-        super();
-    }
-
-    public Admin(String firstName, String lastName, String email) {
-        super(firstName, lastName, email);
-    }
-
-    public static synchronized Admin getInstance() {
-        if (instance == null) {
-            instance = new Admin("Ali" , "Mousavi" , "AliMousavi1234@gmail.com");
-        }
-        return instance;
-    }
+//    private static Admin instance;
+//
+//    protected Admin() {
+//        super();
+//    }
+//
+//    public Admin(String firstName, String lastName, String email) {
+//        super(firstName, lastName, email);
+//    }
+//
+//    public static synchronized Admin getInstance() {
+//        if (instance == null) {
+//            instance = new Admin("Ali" , "Mousavi" , "AliMousavi1234@gmail.com");
+//        }
+//        return instance;
+//    }
 }
