9f35b9b410d0d51d91dfabdfa485d140ff142bfd
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-03 06:03:25 +0330

    implement CommentService.java

---
+++ src/main/java/com/example/phase2/service/CommentService.java
@@ -0,0 +1,16 @@
+package com.example.phase2.service;
+
+import com.example.phase2.entity.Comment;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public interface CommentService {
+    Comment saveOrUpdate(Comment comment);
+    Optional<Comment> findById(Long id);
+    List<Comment> findAll();
+    List<Comment> saveAll(List<Comment> comments);
+    void deleteById(Long id);
+}
