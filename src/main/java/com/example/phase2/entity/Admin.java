299477df7776fec4c86e3b9bbc0d5d6dfa2a47fc
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 05:26:14 +0330

    implement singleton Design pattern for Admin.java

--- src/main/java/com/example/phase2/entity/Admin.java
+++ src/main/java/com/example/phase2/entity/Admin.java
@@ -1,24 +1,37 @@
 package com.example.phase2.entity;
 
 
+import com.example.phase2.entity.enumeration.Role;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
 @Entity
 public class Admin extends User{
-//    private static Admin instance;
-//
-//    protected Admin() {
-//        super();
-//    }
-//
-//    public Admin(String firstName, String lastName, String email) {
-//        super(firstName, lastName, email);
-//    }
-//
-//    public static synchronized Admin getInstance() {
-//        if (instance == null) {
-//            instance = new Admin("Ali" , "Mousavi" , "AliMousavi1234@gmail.com");
-//        }
-//        return instance;
-//    }
+    private static Admin instance;
+
+    protected Admin() {
+        super();
+    }
+
+    private Admin(String firstName, String lastName, String email, String password, Role role, Boolean isEnabled) {
+        super(firstName, lastName, email, password, role, isEnabled);
+    }
+
+    public static synchronized Admin getInstance() {
+        if (instance == null) {
+            instance = new Admin("Ali" , "Mousavi" , "AliMousavi1234@gmail.com" , "1234qwer" , Role.ROLE_ADMIN , true);
+        }
+        return instance;
+    }
 }
