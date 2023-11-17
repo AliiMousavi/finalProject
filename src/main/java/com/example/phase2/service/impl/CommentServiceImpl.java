db01eec0f5e2b0e6691cc76676489fa62aa119ff
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 05:32:45 +0330

    Add CommentServiceImpl.update()

--- src/main/java/com/example/phase2/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/phase2/service/impl/CommentServiceImpl.java
@@ -3,9 +3,11 @@ package com.example.phase2.service.impl;
 import com.example.phase2.entity.Comment;
 import com.example.phase2.repository.CommentRepository;
 import com.example.phase2.service.CommentService;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 @Service
@@ -19,7 +21,17 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     @Transactional
-    public Comment saveOrUpdate(Comment comment) {
+    public Comment saveOrUpdate(Comment comment){
+        try{
+            return commentRepository.save(comment);
+        }catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public Comment update(Comment comment) {
         try{
             return commentRepository.save(comment);
         }catch (Exception e) {
