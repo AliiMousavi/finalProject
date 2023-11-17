45e18a7dd98ca3ba0bda28948a577b0a8a68373f
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 05:53:06 +0330

    add required methods for CustomerRepository.java

--- src/main/java/com/example/phase2/repository/CustomerRepository.java
+++ src/main/java/com/example/phase2/repository/CustomerRepository.java
@@ -2,11 +2,22 @@ package com.example.phase2.repository;
 
 
 import com.example.phase2.entity.Customer;
+import com.example.phase2.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
+public interface CustomerRepository extends JpaRepository<Customer,Long>, JpaSpecificationExecutor<Customer> {
+    Optional<Customer> findCustomerByEmail(String email);
+    Optional<Customer> findCustomerByActivationToken(String activationToken);
+
+    @Query(value = "SELECT c.* FROM Users c LEFT JOIN Orders o ON c.id = o.customer_id "
+            + "GROUP BY c.id HAVING COUNT(o.id) = :numberOfOrders", nativeQuery = true)
+    List<Customer> findByNumberOfOrders(int numberOfOrders);
+
 }
