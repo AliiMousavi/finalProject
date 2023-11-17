62f035f979e754161bff8c8da262f8b19cb3a602
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 05:32:06 +0330

    Add @PreAuthorize for methods in CommentController.java

--- src/main/java/com/example/phase2/controller/CommentController.java
+++ src/main/java/com/example/phase2/controller/CommentController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class CommentController {
     private final CommentServiceImpl commentService;
 
     @PostMapping("/save")
+    @PreAuthorize("hasAnyRole('ADMIN','EXPERT', 'CUSTOMER')")
     public ResponseEntity<CommentResponseDto> save(@RequestBody @Valid CommentRequestDto commentRequestDto){
         Comment comment = CommentMapper.INSTANCE.dtoToComment(commentRequestDto);
         Comment savedComment = commentService.saveOrUpdate(comment);
@@ -29,6 +31,7 @@ public class CommentController {
     }
 
     @GetMapping("/find-by-id/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','EXPERT', 'CUSTOMER')")
     public ResponseEntity<CommentResponseDto> findById(@PathVariable Long id){
         Comment comment = commentService.findById(id).orElseThrow();
         CommentResponseDto commentResponseDto = CommentMapper.INSTANCE.commentToDto(comment);
@@ -36,6 +39,7 @@ public class CommentController {
     }
 
     @GetMapping("/find-all")
+    @PreAuthorize("hasAnyRole('ADMIN','EXPERT', 'CUSTOMER')")
     public ResponseEntity<List<CommentResponseDto>> findAll() {
         List<Comment> comments = commentService.findAll();
         List<CommentResponseDto> commentResponseDtos = comments.stream()
@@ -45,17 +49,19 @@ public class CommentController {
     }
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CommentResponseDto> updateComment(@PathVariable Long id,
                                                                   @RequestBody @Valid CommentRequestDto commentRequestDto) {
         Comment comment = commentService.findById(id).orElseThrow();
         Comment updatedComment = CommentMapper.INSTANCE.dtoToComment(commentRequestDto);
         updatedComment.setId(comment.getId());
-        Comment savedComment = commentService.saveOrUpdate(updatedComment);
+        Comment savedComment = commentService.update(updatedComment);
         CommentResponseDto commentResponseDto =CommentMapper.INSTANCE.commentToDto(savedComment);
         return ResponseEntity.ok(commentResponseDto);
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Long> deleteComment(@PathVariable Long id) {
         commentService.deleteById(id);
         return ResponseEntity.ok(id);
