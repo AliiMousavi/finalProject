9827e1faa5683d54b80fe6c9c618dc4b55d06358
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 06:12:11 +0330

    implement toString() for SubServiceResponseDto_justExpert.java

--- src/main/java/com/example/phase2/dto/SubServiceResponseDto_justExpert.java
+++ src/main/java/com/example/phase2/dto/SubServiceResponseDto_justExpert.java
@@ -15,4 +15,12 @@ import java.util.List;
 @NoArgsConstructor
 public class SubServiceResponseDto_justExpert {
     private List<Expert> experts= new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "SubServiceResponseDto_justExpert{" +
+                "experts=" + experts.stream()
+                .map(Expert::getFirstName)+
+                '}';
+    }
 }
