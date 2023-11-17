eb6feb13bc61e682dacea097605dc68dc9da91f6
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 05:58:11 +0330

    add ExpertService.update()

--- src/main/java/com/example/phase2/service/ExpertService.java
+++ src/main/java/com/example/phase2/service/ExpertService.java
@@ -10,12 +10,15 @@ import java.util.Optional;
 @Service
 public interface ExpertService {
     Expert saveOrUpdate(Expert expert);
+    Expert update(Expert expert);
     Optional<Expert> findById(Long id);
     List<Expert> findAll();
     List<Expert> saveAll(List<Expert> experts);
     void deleteById(Long id);
-    Expert ChangePasswordByID(String password, Long id);
+    Expert ChangePasswordByID(String password);
     List<Order> getOrdersForOffering(Expert expert);
     Offer offering(Offer offer);
     List<Integer> getScoreOfComments(Long ExpertId);
+    Optional<Expert> findExpertByEmail(String email);
+    Optional<Expert> findExpertByActivationToken(String activationToken);
 }
