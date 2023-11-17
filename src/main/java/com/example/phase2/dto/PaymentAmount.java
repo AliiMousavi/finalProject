66d08780c3f4c25bce3f729f37c75bac7f6abb4d
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 06:07:02 +0330

    impelemnt PaymentAmount.java

---
+++ src/main/java/com/example/phase2/dto/PaymentAmount.java
@@ -0,0 +1,14 @@
+package com.example.phase2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentAmount {
+    int paymentAmount;
+}
