06aa21c07c14543f85bb0cc1263a82fa64014755
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 06:05:06 +0330

    add OrderService.update()

--- src/main/java/com/example/phase2/service/OrderService.java
+++ src/main/java/com/example/phase2/service/OrderService.java
@@ -11,8 +11,12 @@ import java.util.Optional;
 @Service
 public interface OrderService {
     Order saveOrUpdate(Order order);
+    Order update(Order order);
     Optional<Order> findById(Long id);
     List<Order> findAll();
     List<Order> saveAll(List<Order> orders);
     void deleteById(Long id);
+    Optional<Order> findOrderByAcceptedOffer_OfferedPrice(int offeredPrice);
+
+    int getPaymentAmountFromOrderID(Long orderId);
 }
