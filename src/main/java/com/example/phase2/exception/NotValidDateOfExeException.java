f1065719616d1335f8a382f94962cbb07a6e7078
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 06:00:27 +0330

    implement NotValidDateOfExeException.java

---
+++ src/main/java/com/example/phase2/exception/NotValidDateOfExeException.java
@@ -0,0 +1,7 @@
+package com.example.phase2.exception;
+
+public class NotValidDateOfExeException extends RuntimeException{
+    public NotValidDateOfExeException(String message) {
+        super(message);
+    }
+}
