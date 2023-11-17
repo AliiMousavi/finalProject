7aaa52a8f3d5d6116d871b96bb66860672929123
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 06:12:28 +0330

    implement SubServiceResponseDtoJustId.java

---
+++ src/main/java/com/example/phase2/dto/SubServiceResponseDtoJustId.java
@@ -0,0 +1,16 @@
+package com.example.phase2.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubServiceResponseDtoJustId {
+    @NotNull
+    private Long id;
+}
