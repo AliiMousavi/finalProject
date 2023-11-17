f5974327e7c565e065d017da877808749fbebaf8
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 06:00:19 +0330

    implement NotMachCaptchaException.java

---
+++ src/main/java/com/example/phase2/exception/NotMachCaptchaException.java
@@ -0,0 +1,7 @@
+package com.example.phase2.exception;
+
+public class NotMachCaptchaException extends RuntimeException {
+    public NotMachCaptchaException(String message) {
+        super(message);
+    }
+}
