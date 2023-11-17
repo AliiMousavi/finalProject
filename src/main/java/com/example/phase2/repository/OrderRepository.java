160a9c6a99247861de5ca0fa5db422e5ad22ea06
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 06:04:18 +0330

    add JpaSpecificationExecutor<Order> to OrderRepository.java

--- src/main/java/com/example/phase2/repository/OrderRepository.java
+++ src/main/java/com/example/phase2/repository/OrderRepository.java
@@ -1,11 +1,16 @@
 package com.example.phase2.repository;
 
 
+import com.example.phase2.entity.Expert;
 import com.example.phase2.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface OrderRepository extends JpaRepository<Order,Long> {
+import java.util.Optional;
 
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> , JpaSpecificationExecutor<Order> {
+
+    Optional<Order> findOrderByAcceptedOffer_OfferedPrice(int offeredPrice);
 }
