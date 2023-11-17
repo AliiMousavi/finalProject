01c2e1ec00162959f2a3cbe28aba278ef027eda8
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 06:01:04 +0330

    add @Getter to Offer.java

--- src/main/java/com/example/phase2/entity/Offer.java
+++ src/main/java/com/example/phase2/entity/Offer.java
@@ -2,38 +2,36 @@ package com.example.phase2.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import lombok.experimental.SuperBuilder;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Table(name = "offers")
 public class Offer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private LocalDateTime DateOfOfferRegister;
+    private LocalDateTime dateOfOfferRegister;
     private int offeredPrice;
     private LocalDateTime offeredDate;
     private int duration;
     @ManyToOne
     private Order order;
+    @OneToOne
+    private Order orderr;
     @ManyToOne
-    Expert expert;
+    private Expert expert;
 
     @Override
     public String toString() {
         return "Offer{" +
                 "id=" + id +
-                ", DateOfOfferRegister=" + DateOfOfferRegister +
+                ", DateOfOfferRegister=" + dateOfOfferRegister +
                 ", offeredPrice=" + offeredPrice +
                 ", offeredDate=" + offeredDate +
                 ", duration=" + duration +
