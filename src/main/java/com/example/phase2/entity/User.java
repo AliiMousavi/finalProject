301dc38f637c47ba72196939517b2377c348a316
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-17 06:14:58 +0330

    add activationToken,activationTokenExpired to User.java

--- src/main/java/com/example/phase2/entity/User.java
+++ src/main/java/com/example/phase2/entity/User.java
@@ -1,19 +1,20 @@
 package com.example.phase2.entity;
 
+import com.example.phase2.entity.enumeration.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.sql.Date;
 import java.time.LocalDateTime;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @SuperBuilder
 @Table(name = "Users")
 public abstract class User{
@@ -24,40 +25,23 @@ public abstract class User{
     private String lastName;
     @Column(unique = true)
     private String email;
-    @Column(length = 8)
     private String password;
     private LocalDateTime dateOfRegistration;
     @Lob
     @Column(name = "image")
     private byte[] image;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private Boolean isEnabled;
+    private String activationToken;
+    private boolean activationTokenExpired;
 
-//    public User(String firstName, String lastName, String email) {
-//    }
-
-//    public User(String firstName, String lastName, String email, byte[] image) throws IOException {
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        if (Validator.isValidEmail(email))
-//            this.email = email;
-//        else
-//            throw new RuntimeException("email is not valid!");
-//        this.password = PasswordGenerator.generatePassword();
-//        this.dateOfRegistration = Date.valueOf(LocalDate.now());
-//        this.credit = 0;
-//        this.image = image;
-//
-//    }
-//
-//    public User(String firstName, String lastName, String email) {
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        if (Validator.isValidEmail(email))
-//            this.email = email;
-//        else
-//            throw new RuntimeException("email is not valid!");
-//        this.password = PasswordGenerator.generatePassword();
-//        this.dateOfRegistration = Date.valueOf(LocalDate.now());
-//        this.credit = 0;
-//    }
-
+    public User(String firstName, String lastName, String email, String password, Role role, Boolean isEnabled) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.isEnabled = isEnabled;
+    }
 }
